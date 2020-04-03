ffa01ac29250c2686e99a3820e01995c54b498fd
Julia Shtohryn <julia.ilkiv@gmail.com>
2020-04-03 22:13:56 +0300

    Updates

---
+++ precache-manifest.0e7a0756e329a186ef741c7658bbdc07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d3d8f35c9248bcd8f28",
+    "url": "/rock-paper-scissors/static/css/main.c3f779fb.chunk.css"
+  },
+  {
+    "revision": "7d3d8f35c9248bcd8f28",
+    "url": "/rock-paper-scissors/static/js/main.7d3d8f35.chunk.js"
+  },
+  {
+    "revision": "780f8d2ff833040905d2",
+    "url": "/rock-paper-scissors/static/css/1.e94667b4.chunk.css"
+  },
+  {
+    "revision": "780f8d2ff833040905d2",
+    "url": "/rock-paper-scissors/static/js/1.780f8d2f.chunk.js"
+  },
+  {
+    "revision": "62f2f0cdf0c221f8d98a",
+    "url": "/rock-paper-scissors/static/js/runtime~main.62f2f0cd.js"
+  },
+  {
+    "revision": "f7eec7aeebf5e63c3773887b6b2930ef",
+    "url": "/rock-paper-scissors/static/media/logo.f7eec7ae.svg"
+  },
+  {
+    "revision": "f57c66fa8d5d8d7a6377fb1ff4a1fb4d",
+    "url": "/rock-paper-scissors/index.html"
+  }
+];
