07d45cb5464010f65951d0703f14366139f9ebab
Julia Shtohryn <julia.ilkiv@gmail.com>
2020-04-03 22:49:34 +0300

    Updates

--- precache-manifest.bd10c093a96bd2351ca6e9b3f9fef2fa.js
+++ precache-manifest.bd10c093a96bd2351ca6e9b3f9fef2fa.js
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/rock-paper-scissors/static/media/logo.f7eec7ae.svg"
   },
   {
-    "revision": "f57c66fa8d5d8d7a6377fb1ff4a1fb4d",
+    "revision": "dbb41c61eb370a0dd8b61348463d4587",
     "url": "/rock-paper-scissors/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rock-paper-scissors/precache-manifest.0e7a0756e329a186ef741c7658bbdc07.js"
+  "/rock-paper-scissors/precache-manifest.bd10c093a96bd2351ca6e9b3f9fef2fa.js"
 );
 
 workbox.clientsClaim();
